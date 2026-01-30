0350f7e8a471ad719043cff5c5214dd3da02cde0
Khushvant Jadhao <khusujadhao329@gmail.com>
2026-01-30 18:55:14 +0530

    Added KYC not verified exception handler

--- src/main/java/com/bank/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bank/exception/GlobalExceptionHandler.java
@@ -1,13 +1,27 @@
 package com.bank.exception;
 
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<String> handleUserExists(UserAlreadyExistsException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
+	@ExceptionHandler(UserAlreadyExistsException.class)
+	public ResponseEntity<String> handleUserExists(UserAlreadyExistsException ex) {
+		return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(InvalidCredentialsException.class)
+	public ResponseEntity<String> handelInvalidCredentialsException(
+			InvalidCredentialsException invalidCredentialsException) {
+		return new ResponseEntity<String>(invalidCredentialsException.getMessage(), HttpStatus.BAD_REQUEST);
+
+	}
+	@ExceptionHandler(KycNotVerifiedException .class)
+	public ResponseEntity<String>handleKycNotVerifiedException(KycNotVerifiedException  kycNotVerifiedException){
+		return new ResponseEntity<String>(kycNotVerifiedException.getMessage(),HttpStatus.NOT_FOUND);
+		
+	}
 }
